3f19a9bf4ae83965ca2a445af6f3c76147d70814
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-06-17 23:36:43 +0200

    Setup: response headers

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 const port = 3001;
 
 app.post('/', (request, response) => {
+app.use(express.json());
+
+app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN);
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    response.header('Access-Control-Allow-Methods', 'POST');
+    next();
+});
 });
 
 app.listen(port);
